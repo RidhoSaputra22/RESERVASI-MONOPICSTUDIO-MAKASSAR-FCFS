5caed8b83807a86c12aa7c73abe974e72e0dc2ac
ridho <ridho@gmail.com>
2026-01-28 21:17:28 +0800

    -

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
     <td class="header">
         <a href="{{ $url }}" target="_blank">
             <img src="{{ asset('images/logo.png') }}" alt="{{ config('app.name') }}"
-                style="max-width: 100%; height: auto;">
+                style="max-width: 50%; height: auto;">
         </a>
 
     </td>
